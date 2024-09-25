b05dd563255ea61b0b779885efe2baf382308150
Chris Pfohl <christopher.pfohl@gmail.com>
2024-09-25 15:53:56 -0400

    Fix duration typing

--- src/ms.ts
+++ src/ms.ts
@@ -1,10 +1,10 @@
-import { units } from './lib/units'
+import { Duration, units } from './lib/units'
 
 // FUNCTION: get number of seconds from a duration string
-export const ms = (duration: string | number): number => {
+export const ms = (duration: Duration | number): number => {
   if (+duration) return +duration
   // @ts-ignore
   const [, value, unit] = duration.match(/^([^ ]+) +(\w\w*?)s?$/) || []
 
-  return +value * (units[unit] || 1)
+  return +value * (units[unit as keyof typeof units] || 1)
 }
